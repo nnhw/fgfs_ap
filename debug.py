d9c8a32250a721edbed03cf2231718a58fa8851f
Илья Куклев <nnhw667@gmail.com>
2018-04-10 23:31:34 +0300

    Debug blessings output

--- debug.py
+++ debug.py
@@ -1,8 +1,23 @@
 from connection import connection_debug
+from blessings import Terminal
 import time
 
 connection_interface = connection_debug(l_type="in", l_port_in=9002)
 
+t = Terminal()
+
+pitch = 0
+roll = 0
+yaw = 0
+
 while True:
     time.sleep(0.1)
-    print(connection_interface.receive_data())
+    print(t.clear())
+    with t.location(int(t.width/2), 0):
+        print('Debugger')
+    with t.location(0, t.height-2):
+        print(time.ctime())
+    pitch, roll, yaw = connection_interface.receive_data()
+    print(t.bold("Elevator = ") + '{0: .5f}'.format(pitch))
+    print(t.bold("Aileron = ") + '{0: .5f}'.format(roll))
+    print(t.bold("Rudder = ") + '{0: .5f}'.format(yaw))
